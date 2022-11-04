e12cfb0ec61a1f43c9ac93865c295c0a5e0bc4a0
Seyed Moein Saadati <saadati.moin@gmail.com>
2022-11-04 13:39:53 +0330

    Re-factor EasyGizmosView.cs Implement custom inspector view for EasyGizmosView.cs Add General Properties

--- Assets/EasyGizmos/Scripts/EasyGizmosView.cs
+++ Assets/EasyGizmos/Scripts/EasyGizmosView.cs
@@ -1,85 +1,98 @@
 ﻿using UnityEngine;
-using NaughtyAttributes;
 
 namespace EasyGizmos
 {
     public class EasyGizmosView : MonoBehaviour
     {
 #if UNITY_EDITOR
-        public enum GizmosShapes { None = 0, Sphere = 1, Cube = 2, WireSphere = 3, WireCube = 4 }
-        [BoxGroup("General")][Label("Color")] public Color generalColor = Color.white;
-        [BoxGroup("General")] public bool drawOnItself = true;
-        [BoxGroup("General")] public bool drawOnChilds = false;
+        public enum GizmosShapes
+        {
+            None = 0,
+            Sphere = 1,
+            Cube = 2,
+            WireSphere = 3,
+            WireCube = 4,
+            Custom = 5
+        }
 
-        [BoxGroup("Shape")][Label("Show")] public bool showSape = true;
-        [BoxGroup("Shape")][ShowIf("showSape")] public GizmosShapes shape = GizmosShapes.Sphere;
-        [BoxGroup("Shape")][ShowIf("showSape")] public float shapeRadius = 0.3f;
-        [BoxGroup("Shape")][ShowIf("showSape")][Label("Sync With Collider")] public bool syncShapeWithCollider = false;
-        [BoxGroup("Shape")][ShowIf("showSape")][Label("Override Color")] public bool overrideShapeColor = false;
-        [BoxGroup("Shape")][ShowIf(EConditionOperator.And, "showSape", "overrideShapeColor")][Label("Color")] public Color shapeColor = Color.white;
-        [BoxGroup("Shape")][ShowIf("showSape")][Label("Offset")] public Vector3 shapeOffset = Vector3.zero;
+        public Color generalColor = Color.white;
+        public bool drawOnItself = true;
+        public bool drawOnChildren = false;
 
-        [BoxGroup("Text")][Label("Show")] public bool showText = false;
-        [BoxGroup("Text")][ShowIf("showText")] public bool overrideText = false;
-        [BoxGroup("Text")][ShowIf(EConditionOperator.And, "showText", "overrideText")][TextArea] public string text = string.Empty;
-        [BoxGroup("Text")][ShowIf("showText")][Label("Size")] public int textFontSize = 20;
-        [BoxGroup("Text")][ShowIf("showText")][Label("Override Color")] public bool overrideTextColor = false;
-        [BoxGroup("Text")][ShowIf(EConditionOperator.And, "showText", "overrideTextColor")][Label("Color")] public Color textColor = Color.white;
-        [BoxGroup("Text")][ShowIf("showText")][Label("Offset")] public Vector3 textOffset = new Vector3(0, 1, 0);
+        public bool showShape = true;
+        public GizmosShapes shape = GizmosShapes.Sphere;
+        public Mesh mesh;
+        public float shapeRadius = 0.3f;
+        public bool syncShapeWithCollider = false;
+        public bool overrideShapeColor = false;
+        public Color shapeColor = Color.white;
+        public Vector3 shapeOffset = Vector3.zero;
 
-        [BoxGroup("Icon")][Label("Show")] public bool showIcon = false;
-        [BoxGroup("Icon")][ShowIf("showIcon")] public Sprite icon = null;
-        [BoxGroup("Icon")][ShowIf("showIcon")][Label("Offset")] public Vector3 iconOffset = Vector3.zero;
+        public bool showText = false;
+        public bool overrideText = false;
+        [TextArea] public string text = string.Empty;
+        public int textFontSize = 20;
+        public bool overrideTextColor = false;
+        public Color textColor = Color.white;
+        public Vector3 textOffset = new Vector3(0, 1, 0);
 
-        [BoxGroup("Line")][Label("Show")] public bool showLine = false;
-        [BoxGroup("Line")][ShowIf("showLine")][Label("Start Point")] public Transform lineStartPoint = null;
-        [BoxGroup("Line")][ShowIf("showLine")][Label("End Point")] public Transform lineEndPoint = null;
-        [BoxGroup("Line")][ShowIf("showLine")][Label("Offset")] public Vector3 lineOffset = Vector3.zero;
-        [BoxGroup("Line")][ShowIf(EConditionOperator.And, "showLine", "overrideLineColor")][Label("Color")] public Color lineColor = Color.white;
-        [BoxGroup("Line")][ShowIf("showLine")][Label("Override Color")] public bool overrideLineColor = false;
+        public bool showIcon = false;
+        public Sprite icon = null;
+        public Vector3 iconOffset = Vector3.zero;
 
-        [BoxGroup("Frustum")][Label("Show")] public bool showFrustum = false;
-        [BoxGroup("Frustum")][ShowIf("showFrustum")][Label("Fov")] public float frustumFov = 60f;
-        [BoxGroup("Frustum")][ShowIf("showFrustum")][Label("Aspect")] public float frustumAspect = 1;
-        [BoxGroup("Frustum")][ShowIf("showFrustum")][Label("Min Range")] public float frustumMinRange = 0.3f;
-        [BoxGroup("Frustum")][ShowIf("showFrustum")][Label("Max Range")] public float frustumMaxRange = 2f;
-        [BoxGroup("Frustum")][ShowIf("showFrustum")][Label("Override Color")] public bool overrideFrustumColor = false;
-        [BoxGroup("Frustum")][ShowIf(EConditionOperator.And, "showFrustum", "overrideFrustumColor")][Label("Color")] public Color frustumColor = Color.white;
+        public bool showLine = false;
+        public Transform lineStartPoint = null;
+        public Transform lineEndPoint = null;
+        public Vector3 lineOffset = Vector3.zero;
+        public Color lineColor = Color.white;
+        public bool overrideLineColor = false;
 
+        public bool showFrustum = false;
+        public float frustumFov = 60f;
+        public float frustumAspect = 1;
+        public float frustumNearPlane = 0.3f;
+        public float frustumFarPlane = 2f;
+        public bool overrideFrustumColor = false;
+        public Color frustumColor = Color.white;
 
         private void OnDrawGizmos()
         {
             if (!IsActive) return;
 
-            DrawSelf();
-            DrawChilds();
+            if (drawOnItself)
+                Draw(transform);
+
+            if (drawOnChildren)
+                DrawChildren();
+
+            if (showLine)
+                ShowLine();
         }
 
-        private void DrawSelf()
+        private void Draw(Transform target)
         {
-            if (!drawOnItself) return;
-            ShowShape(transform);
-            ShowText(transform);
-            ShowIcon(transform);
-            ShowLine();
-            ShowFrustum(transform);
+            if (showShape)
+                ShowShape(target);
+
+            if (showText)
+                ShowText(target);
+
+            if (showIcon)
+                ShowIcon(target);
+
+            if (showFrustum)
+                ShowFrustum(target);
         }
 
-        private void DrawChilds()
+        private void DrawChildren()
         {
-            if (!drawOnChilds) return;
             for (int i = 0; i < transform.childCount; i++)
-            {
-                ShowShape(transform.GetChild(i));
-                ShowText(transform.GetChild(i));
-                ShowIcon(transform.GetChild(i));
-                ShowFrustum(transform.GetChild(i));
-            }
+                Draw(transform.GetChild(i));
         }
 
         private GUIStyle GetTextStyle()
         {
-            var style = new GUIStyle { fontSize = textFontSize };
+            var style = new GUIStyle {fontSize = textFontSize};
             style.normal.textColor = overrideTextColor ? textColor : generalColor;
             style.richText = true;
             return style;
@@ -87,8 +100,6 @@ namespace EasyGizmos
 
         private void ShowShape(Transform baseTransform)
         {
-            if (!showSape) return;
-
             var sc = GetComponent<SphereCollider>();
             var bc = GetComponent<SphereCollider>();
 
@@ -108,33 +119,33 @@ namespace EasyGizmos
                     Gizmos.DrawSphere(baseTransform.position + shapeOffset, shapeRadius);
                     break;
                 case GizmosShapes.Cube:
-                    Gizmos.DrawCube(baseTransform.position + shapeOffset, new Vector3(shapeRadius * 2, shapeRadius * 2, shapeRadius * 2));
+                    Gizmos.DrawCube(baseTransform.position + shapeOffset,
+                        new Vector3(shapeRadius * 2, shapeRadius * 2, shapeRadius * 2));
                     break;
                 case GizmosShapes.WireSphere:
                     Gizmos.DrawWireSphere(baseTransform.position + shapeOffset, shapeRadius);
                     break;
                 case GizmosShapes.WireCube:
-                    Gizmos.DrawWireCube(baseTransform.position + shapeOffset, new Vector3(shapeRadius * 2, shapeRadius * 2, shapeRadius * 2));
+                    Gizmos.DrawWireCube(baseTransform.position + shapeOffset,
+                        new Vector3(shapeRadius * 2, shapeRadius * 2, shapeRadius * 2));
                     break;
             }
         }
 
         private void ShowText(Transform baseTransform)
         {
-            if (!showText) return;
             UnityEditor.Handles.Label(baseTransform.position + textOffset, overrideText ? text : name, GetTextStyle());
         }
 
         private void ShowIcon(Transform baseTransform)
         {
-            if (!showIcon) return;
             if (icon == null) return;
-            Gizmos.DrawIcon(baseTransform.position + iconOffset, UnityEditor.AssetDatabase.GetAssetPath(icon.GetInstanceID()));
+            Gizmos.DrawIcon(baseTransform.position + iconOffset,
+                UnityEditor.AssetDatabase.GetAssetPath(icon.GetInstanceID()));
         }
 
         private void ShowLine()
         {
-            if (!showLine) return;
             if (lineStartPoint == null) return;
             if (lineEndPoint == null) return;
             Gizmos.color = overrideLineColor ? lineColor : generalColor;
@@ -143,10 +154,9 @@ namespace EasyGizmos
 
         private void ShowFrustum(Transform baseTransform)
         {
-            if (!showFrustum) return;
             Gizmos.color = overrideFrustumColor ? frustumColor : generalColor;
             Gizmos.matrix = Matrix4x4.TRS(baseTransform.position, baseTransform.rotation, Vector3.one);
-            Gizmos.DrawFrustum(Vector3.zero, frustumFov, frustumMaxRange, frustumMinRange, frustumAspect);
+            Gizmos.DrawFrustum(Vector3.zero, frustumFov, frustumFarPlane, frustumNearPlane, frustumAspect);
         }
 
         //////////////////////////////////////////////////////
--- Assets/EasyGizmos/Scripts/Editor/EasyGizmosEditor.cs
+++ Assets/EasyGizmos/Scripts/Editor/EasyGizmosEditor.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 namespace EasyGizmos
 {
-    [InitializeOnLoad]
+    [InitializeOnLoad, CustomEditor(typeof(EasyGizmosView))]
     public class EasyGizmosEditor : Editor
     {
+        private EasyGizmosView context;
+
+        private void OnEnable()
+        {
+            context = (EasyGizmosView) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            // General Properties
+
+            EditorGUILayout.LabelField("General");
+            context.generalColor = EditorGUILayout.ColorField("Color", context.generalColor);
+            context.drawOnItself = EditorGUILayout.Toggle("Draw On Itself", context.drawOnItself);
+            context.drawOnChildren = EditorGUILayout.Toggle("Draw On Children", context.drawOnChildren);
+
+            // Shape Properties
+            // Text Properties
+            // Icon Properties
+            // Line Properties
+            // Frustum Properties
+        }
+
+
+        //////////////////////////////////////////////////////
+        /// STATIC MEMBERS
+        //////////////////////////////////////////////////////
         static EasyGizmosEditor()
         {
             SceneView.duringSceneGui -= OnDuringSceneGui;
@@ -17,8 +46,8 @@ namespace EasyGizmos
         {
             Handles.BeginGUI();
             {
-                var sceneWidth = ((EditorWindow)obj).position.width;
-                var rect = new Rect((sceneWidth - 150) / 2, 0, 150, 70);
+                var sceneWidth = obj.position.width;
+                var rect = new Rect((sceneWidth - 150) / 2, 5, 150, 70);
                 GUILayout.BeginArea(rect);
                 {
                     var showHide = EasyGizmosView.IsActive ? "Hide" : "Show";
@@ -35,14 +64,15 @@ namespace EasyGizmos
             switch (e.type)
             {
                 case EventType.KeyDown:
+                {
+                    if (Event.current.keyCode == KeyCode.G)
                     {
-                        if (Event.current.keyCode == (KeyCode.G))
-                        {
-                            EasyGizmosView.IsActive = !EasyGizmosView.IsActive;
-                            e.Use();
-                        }
-                        break;
+                        EasyGizmosView.IsActive = !EasyGizmosView.IsActive;
+                        e.Use();
                     }
+
+                    break;
+                }
             }
         }
     }
